77f4ae53e0fa2d36766734cad877917f4969ac33
KamalSharma13 <kamal.webdev13@gmail.com>
2026-01-12 00:15:29 +0530

    updated

--- server/common/server.js
+++ server/common/server.js
@@ -50,9 +50,9 @@ const corsOptions = {
     if (allowedOrigins.includes(origin)) return cb(null, true);
     return cb(new Error(`CORS blocked: ${origin}`));
   },
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"],
   allowedHeaders: ["Content-Type", "Authorization","Accept"],
-  credentials: true,
 };
 
 app.use(cors(corsOptions));
